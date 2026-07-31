647454144921c993222e237a83c84ffaf483b3fb
limoxd <caplego24@gmail.com>
2026-07-31 17:03:52 -0400

    Change in a way how to get speed, i don't think is right

--- src/main/java/com/limoxd/horseengine/events/HorseInteractingEvent.java
+++ src/main/java/com/limoxd/horseengine/events/HorseInteractingEvent.java
@@ -3,6 +3,7 @@ package com.limoxd.horseengine.events;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.animal.horse.Horse;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -23,9 +24,11 @@ public class HorseInteractingEvent {
         if (!(interaction.getTarget() instanceof Horse)){return;}
 
         Player player = interaction.getEntity();
+        Horse horse = (Horse) interaction.getTarget();
 
         if (player.getItemInHand(InteractionHand.MAIN_HAND).is(Items.LAVA_BUCKET)){
             player.sendSystemMessage(Component.literal("it works"));
+            player.sendSystemMessage(Component.literal(String.valueOf(horse.getAttribute(Attributes.MOVEMENT_SPEED))));
         }
 
         interaction.setCancellationResult(InteractionResult.SUCCESS);
