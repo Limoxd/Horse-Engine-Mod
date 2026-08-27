406361904b633d2c9c9ddc93b044f857a2d573bb
limoxd <caplego24@gmail.com>
2026-08-26 23:04:11 -0400

    Cleaned some parts and added oritech como depedencia opcional

--- src/main/java/com/limoxd/horseengine/util/HorseEngineTags.java
+++ src/main/java/com/limoxd/horseengine/util/HorseEngineTags.java
@@ -1,12 +1,10 @@
 package com.limoxd.horseengine.util;
 
 import com.limoxd.horseengine.HorseEngines;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
-import net.neoforged.bus.api.IEventBus;
 
 public class HorseEngineTags {
     public static class Items {
